4079fc3c5646f36b29d44d370f4ac0d5f301b0a2
Davis <52375250+davis-b@users.noreply.github.com>
2019-07-11 19:06:00 -0400

    Remove comments containing inotify api mask information

--- follow.zig
+++ follow.zig
@@ -28,45 +28,6 @@
 // Then on each new command, we would see if the last command's pid is running,
 // and kill it if so.
 
-// Inotify watch flags / masks. Courtesy of "https://linux.die.net/man/7/inotify"
-//    IN_ACCESS
-//    File was accessed (read) (*).
-//
-//    IN_ATTRIB
-//    Metadata changed, e.g., permissions, timestamps, extended attributes, link count (since Linux 2.6.25), UID, GID, etc. (*).
-//
-//    IN_CLOSE_WRITE
-//    File opened for writing was closed (*).
-//
-//    IN_CLOSE_NOWRITE
-//    File not opened for writing was closed (*).
-//
-//    IN_CREATE
-//    File/directory created in watched directory (*).
-//
-//    IN_DELETE
-//    File/directory deleted from watched directory (*).
-//
-//    IN_DELETE_SELF
-//    Watched file/directory was itself deleted.
-//
-//    IN_MODIFY
-//    File was modified (*).
-//
-//    IN_MOVE_SELF
-//    Watched file/directory was itself moved.
-//
-//    IN_MOVED_FROM
-//    File moved out of watched directory (*).
-//
-//    IN_MOVED_TO
-//    File moved into watched directory (*).
-//
-//    IN_OPEN
-//    File was opened (*).
-
-//    When monitoring a directory, the events marked with an asterisk (*) above can occur for files in the directory, in which case the name field in the returned inotify_event structure identifies the name of the file within the directory.
-
 const std = @import("std");
 const os = std.os;
 const dict = std.hash_map;
